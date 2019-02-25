8fa63a9fdb01d38ad65b86692ed93d9504523427
Floriane Didier <florianedidier@gmail.com>
2019-02-25 17:23:57 -0300

    the longest word game: finished

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = []
-    10.times { @letters << ('a'..'z').to_a.sample }
+    10.times { @letters << ('A'..'Z').to_a.sample }
   end
 
   def score
-    params[:word]
+    @word = params[:word]
+    array_letters = params[:letters].split(" ")
+    if english_word(@word)["found"] == false
+      @message = "Sorry but #{@word} does not seem to be a valid English word."
+    elsif @word.upcase.split("").all? { |letter| array_letters.include?(letter) } == false
+      @message = "Sorry but #{@word} cannot be built out of the grid"
+    else
+      @message = "Congratulations! #{@word} is a valid English word"
+    end
+  end
+
+  def english_word(attempt)
+    url = 'https://wagon-dictionary.herokuapp.com/' + attempt
+    wagon_dico = open(url).read
+    return JSON.parse(wagon_dico) # it is a hash
   end
 end
